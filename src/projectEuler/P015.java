2041f2c740f9da19a30136b605c16a31faeb7e03
Diego <diego.rodriguesazevedo@gmail.com>
2015-10-20 23:40:15 -0200

    Real math boring solution.

--- src/projectEuler/P015.java
+++ src/projectEuler/P015.java
@@ -1,5 +1,7 @@
 package projectEuler;
 
+import java.math.BigDecimal;
+
 /**
  * Starting in the top left corner of a 2x2 grid, and only being able to move 
  * to the right and down, there are exactly 6 routes to the bottom right corner.
@@ -14,11 +16,12 @@ package projectEuler;
 public class P015 {
 	
 	private static final int GRID_SIZE = 20;
-	private static final int SMALL_GRID_SIZE = 2;
+	private static final int SMALL_GRID_SIZE = 14;
 	
 	public static void main(String[] args) {
 		System.out.println(numberOfSteps(0, 0));
 		System.out.println(2*halfOfTheSteps(0, 0));
+		System.out.println(binomial(GRID_SIZE*2, GRID_SIZE));
 	}
 	
 	/* Almost 10 minutes running. Not the used algorithm, but was the first.
@@ -42,6 +45,23 @@ public class P015 {
 		}
 		
 		return numberOfSteps(x+1, y)+numberOfSteps(x, y+1);
+	}
+	
+	/* Real math solution is a binomial:
+	 * a!/(a-b)!b!
+	 * a*(a-1)*...*(b+1)/(a-b)!
+	 * Had to use BigDecimal because the 40*39*...*21 is way too big to fit a Long.
+	 */
+	private static BigDecimal binomial(int a, int b){
+		return binomial(a-1, b, a-b).multiply(BigDecimal.valueOf(a)).divide(fatorial(a-b));
+	}
+	
+	private static BigDecimal binomial(int a, int b, int c){
+		return (a==b)? BigDecimal.ONE :binomial(a-1, b, c).multiply(BigDecimal.valueOf(a));
+	}
+	
+	private static BigDecimal fatorial(int a){
+		return (a==1)?BigDecimal.ONE:fatorial(a-1).multiply(BigDecimal.valueOf(a));
 	}
 
 }
